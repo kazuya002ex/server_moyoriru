8db36fd7d0b0a2614b8fe5ec013397aed16df534
kazuya002ex <chiharu.event2@gmail.com>
2021-09-01 00:28:52 +0900

    feat: venue#showに埋め込み

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,5 @@
 class VenuesController < ApplicationController
-  # TODO: Controller実装後に戻す
-  # before_action :set_venue, only: %i(show)
+  before_action :set_venue, only: %i(show)
 
   def index
   end
